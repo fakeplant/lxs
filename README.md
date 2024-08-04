10501cb9fdd1f877a36b664cd1ce452d9d58892e
plant <fake@plant.dev>
2024-08-03 19:05:11 -0700

    init

---
+++ README.md
@@ -0,0 +1,59 @@
+# LXS
+Used to to backfill [Chromatik](https://chromatik.co/) model file (.lxm) changes back to original fixture files (.lxf).
+
+## Install
+
+1. Install the dependencies:
+
+```bash
+$ npm install
+```
+
+2. Rename `.env.example` to `.env` and fill in the required values.
+
+
+3. Build the project:
+
+```bash
+$ npm run build
+```
+
+4. Link the package:
+
+```bash
+$ npm link
+```
+
+
+## Usage
+
+Basic usage with no arguments:
+```bash
+$ lxs sync
+```
+
+
+## Development
+
+
+
+### Running the project
+
+```bash
+$ npx tsx src/index.ts <command>
+```
+
+### Building
+
+To build the project:
+
+```bash
+$ npm run build
+```
+
+### Formatting
+The code is formatted using [Prettier](https://prettier.io/). 
+
+Settings are found in the `.prettierrc.json` file.
+
+Setup automatic formatting in your editor by following the instructions [here](https://prettier.io/docs/en/editors.html)
---
+++ src/index.ts
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+
+import { Command } from "commander"
+import packageJson from "../package.json"
+import * as fs from "node:fs"
+import "dotenv/config"
+import JSON5 from "json5"
+import * as fleece from "golden-fleece"
+
+// Define program
+const program = new Command()
+program
+  .name(packageJson.name)
+  .description(packageJson.description)
+  .version(packageJson.version)
+  .showHelpAfterError()
+
+// Create 'sync' command
+program
+  .command("sync")
+  .description("Sync fixture with model.")
+  .option("-m, --model <path>", "path to the model file")
+  .option("-f, --fixtures <path>", "path to the fixtures directory")
+  .action((options) => {
+    // Handle options
+    const optionsModelPath = options.model
+    const optionsFixturesPath = options.fixtures
+    const modelPath = optionsModelPath
+      ? optionsModelPath
+      : process.env.MODEL_FILE_PATH
+    const fixturesPath = optionsFixturesPath
+      ? optionsFixturesPath
+      : process.env.FIXTURES_DIR_PATH
+
+    // Validate paths are provided
+    if (!modelPath) {
+      console.log(
+        "Include --model option to command or set MODEL_FILE_PATH environment variable in .env file"
+      )
+      process.exit(1)
+    }
+    if (!fixturesPath) {
+      console.log(
+        "Include --fixtures option to command or set FIXTURE_DIR_PATH environment variable in .env file"
+      )
+      process.exit(1)
+    }
+
+    // Validate paths are valid files/directories
+    try {
+      fs.accessSync(modelPath, fs.constants.F_OK)
+    } catch (error) {
+      console.error(`Model file does not exist at path: ${modelPath}`)
+      process.exit(1)
+    }
+    try {
+      fs.accessSync(fixturesPath, fs.constants.F_OK)
+    } catch (error) {
+      console.error(
+        `Fixtures directory does not exist at path: ${fixturesPath}`
+      )
+      process.exit(1)
+    }
+
+    // Get model file
+    const modelFile = fs.readFileSync(modelPath, "utf8") as any
+    const modelFileJson = JSON5.parse(modelFile)
+
+    // Iterate through fixture files and store changes
+    const modelFixtures = modelFileJson.fixtures
+    for (const modelFixture of modelFixtures) {
+      // Get Fixture file
+      const fixturePath = modelFixture.jsonFixtureType
+      const fixtureFile = fs.readFileSync(
+        `${fixturesPath}/${fixturePath}.lxf`,
+        "utf8"
+      )
+      const fixtureFileJson = JSON5.parse(fixtureFile)
+
+      // Compare fixture file params
+      let isChanged = false
+      const modelFixtureParams = modelFixture.jsonParameters
+      const fixtureParams = fixtureFileJson.parameters
+      for (const fixtureParam of Object.entries(fixtureParams)) {
+        const fixtureParamKey = fixtureParam[0]
+        const fixtureParamObj = fixtureParam[1] as any
+        const fixtureParamDefaultValue = fixtureParamObj.default
+        const matchingModelFixtureParam = modelFixtureParams[fixtureParamKey]
+
+        if (fixtureParamDefaultValue !== matchingModelFixtureParam) {
+          console.log(
+            `Updating fixture param: ${fixturePath} - ${fixtureParamKey}: ${fixtureParamDefaultValue} -> ${matchingModelFixtureParam}`
+          )
+          fixtureParamObj.default = matchingModelFixtureParam
+          isChanged = true
+        }
+      }
+
+      // Write changes to fixture file
+      // Use 'fleece' to preserve JSON5 formatting
+      if (isChanged) {
+        const file = fleece.patch(fixtureFile, fixtureFileJson)
+        fs.writeFileSync(`${fixturesPath}/${fixturePath}.lxf`, file)
+      }
+    }
+  })
+
+// Run program
+program.parse(process.argv)
